92de24df2f39f92467f96bbcf4bc59e4ec4e17fd
alexbook00 <43623251+alexbook00@users.noreply.github.com>
2022-02-23 17:28:48 -0700

    Add files via upload

--- Homework/HW2/random_walks_02_28_19_part2.py
+++ Homework/HW2/random_walks_02_28_19_part2.py
@@ -10,15 +10,16 @@ class random_walks_python():
         N = 500  # no of steps per trajectory
         realizations = 50  # number of trajectories
         v = 1.0  # velocity (step size)
-        theta_s_array = [round(math.pi / 24, 4), round(math.pi / 12, 4), round(math.pi / 3, 4)]  # the width of the random walk turning angle distribution (the lower it is, the more straight the trajectory will be)
-        w_array = [0.0, 0.5, 1.0]  # w is the weighting given to the directional bias (and hence (1-w) is the weighting given to correlated motion)
-        ratio_theta_s_brw_crw = 1
+        theta_s_array = [round(math.pi / 3, 4)]  # the width of the random walk turning angle distribution (the lower it is, the more straight the trajectory will be)
+        w_array = np.arange(0, 1.001, .001)  # w is the weighting given to the directional bias (and hence (1-w) is the weighting given to correlated motion)
+        ratio_theta_s_brw_crw = .1
         plot_walks = 0
         count = 0
 
-        efficiency_array = np.zeros([len(theta_s_array), len(w_array), N])
+        efficiency_array = np.zeros(len(w_array))
         for w_i in range(len(w_array)):
             w = w_array[w_i]
+            print(w)
             for theta_s_i in range(len(theta_s_array)):
                 theta_s_crw = np.multiply(ratio_theta_s_brw_crw, theta_s_array[theta_s_i])
                 theta_s_brw = theta_s_array[theta_s_i]
@@ -28,32 +29,16 @@ class random_walks_python():
                     plt.figure(count)
                     plt.plot(x.T, y.T)
                     plt.axis('equal')
-                efficiency_over_time_steps = np.zeros(500)
-                for i in range(N):
-                    efficiency_over_time_steps[i] = np.divide(np.mean(x[:, i] - x[:, 0]), (v * i))
-                efficiency_array[theta_s_i, w_i] = efficiency_over_time_steps
+                efficiency = np.divide(np.mean(x[:, -1] - x[:, 0]), (v * N))
+                efficiency_array[w_i] = efficiency
             # plt.show()
-        plt.figure()
-        legend_array = []
-        for theta_s_i in range(0, len(theta_s_array)):
-            for w_i in range(len(w_array)):
-                legend_array.append(
-                    ["$\theta^{*CRW}=$", (ratio_theta_s_brw_crw * theta_s_array[theta_s_i]),
-                     "$\theta^{*BRW}=$", (theta_s_array[theta_s_i]),
-                     "$w=$", w_array[w_i]]
-                )
 
-        plt.title('Navigational Efficiency vs. Time')
-        plt.xlabel('Time Step')
+        plt.figure()
+
+        plt.title("Navigational Efficiency vs. $w$")
+        plt.xlabel("$w$ (directional bias weighting)")
         plt.ylabel('Navigational Efficiency')
-        colors = ['b', 'g', 'r', 'b', 'g', 'r', 'b', 'g', 'r']
-        linestyles = ['solid', 'solid', 'solid', 'dotted', 'dotted', 'dotted', 'dashed', 'dashed', 'dashed']
-        for efficiency, color, label, linestyle in zip(
-                                                        np.reshape(efficiency_array,(len(theta_s_array)*len(w_array), N)),
-                                                        colors,
-                                                        legend_array,
-                                                        linestyles):
-            plt.plot(range(N), efficiency, color, label=label, linestyle=linestyle, alpha=.75)
+        plt.plot(w_array, efficiency_array, 'b', alpha=.75)
         plt.legend(loc='lower right', prop={'size': 8})
         plt.show()
 
